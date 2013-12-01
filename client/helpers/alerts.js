acc9ad23316481e46e97f86400c1bbd77d98f01d
Louis Bataillard <info@mobweb.ch>
2013-12-01 21:17:25 +0100

    changed the alert helper to only display one alert at a time && added a "pulse" when an alert is displayed

--- client/helpers/alerts.js
+++ client/helpers/alerts.js
@@ -16,11 +16,23 @@ Alerts = new Meteor.Collection(null);
 
 // Creating a new alert
 showAlert = function(message, type) {
+	// Before showing a new alert, remove any existing old ones
+	clearAlerts();
+
+	// Default alert type
 	if(!type) {
 		type = 'error';
 	}
 
+	// Finally insert the alert into the Alerts collection
 	Alerts.insert({message: message, type: type, seen: false});
+
+	// Make the alert "pulse" quickly so the user can see it
+	// (This also comes handy if the same alert is displayed twice
+	// in a row, e.g. when creating a new card)
+	window.setTimeout(function() {
+		$('.alert').fadeOut(200).fadeIn(200);
+	}, 100);
 };
 
 // Wiping out all the viewed alerts
